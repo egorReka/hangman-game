4065b164c0771f91d83ecb1d5ae7060f5007227a
egorReka <e.reka55@yandex.ru>
2024-01-07 19:23:33 +0300

    feat: connect fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./idex.html', './src/**/*.js'],
+  content: [
+    "./index.html",
+    "./src/**/*.js"
+  ],
   theme: {
     extend: {},
   },
