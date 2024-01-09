42e62eadc8f2d8464ed9c10af260b2cc84a736be
egorReka <e.reka55@yandex.ru>
2024-01-09 13:05:43 +0300

    fix: corrects names

--- src/js/create-placeholders.js
+++ src/js/create-placeholders.js
@@ -1,11 +1,9 @@
-const gameContainer = document.querySelector('#game');
-
-const renderPlaceholders = (word) => {
+const createPlaceholders = (word) => {
   const fragment = document.createDocumentFragment();
   const newListElement = document.createElement('ul');
 
   newListElement.id = 'placeholders';
-  newListElement.classList.add('placeholders-wrapper')
+  newListElement.classList.add('placeholders-wrapper');
 
   for (let i = 0; word.length > i; i++) {
     const newItemElement = document.createElement('li');
@@ -13,13 +11,12 @@ const renderPlaceholders = (word) => {
     newItemElement.textContent = '_';
     newItemElement.id = `letter_${i}`;
     newItemElement.classList.add('letter');
-
     newListElement.append(newItemElement);
   };
 
-  gameContainer.innerHTML = '';
   fragment.append(newListElement);
-  return gameContainer.append(fragment);
+
+  return fragment;
 }
 
-export { renderPlaceholders };
+export { createPlaceholders };
